d1ce89cfbdaead21f568dbc22541e8ea399a52f1
Adem Erbas <ae@netsense.no>
2023-08-07 01:19:57 +0200

    merged-again

--- svelte-temelleri-udemy/svelte14/src/routes/request.ts
+++ svelte-temelleri-udemy/svelte14/src/routes/request.ts
@@ -1,5 +1,4 @@
 
-<<<<<<< HEAD
 
 /*Herzaman json data gondermek istemeyebiliriz bazen, formData da gondermek 
 isteyebiliriz onun icinde bir fonksiyon yazacak olursak 
@@ -48,25 +47,11 @@ data post methodunda gonderilecek data, diyoruz, amna her zaman post
 function request(url:string, method="POST", data:any=false):Promise<any>
 {
 	return new Promise((resolve:any,reject:any)=>{
-=======
-interface optionsType 
-{
-	method:string;
-	body?:string;
-}
-
-//data post methodunda gonderilecek data, diyoruz, amna her zaman post methodunda data gondermeyebiliriz onun icin false ise data gonderilmeyen bir post methodu diye algiliyoruz, biz bazen methodumuz post olur ve sadece token gonderirirz, ama islemizmiz aslinda uzak api-endpointinden protected bir data  yi cekkip ekrana basmaktir, yani herhangi bir crate,update, delete yapmasak bile...
-function request(url:string, method="POST", data:any=false):Promise<any>
-{
-	return new Promise((resolve:any,reject:any)=>{
-
->>>>>>> 8382b31d499b82bef99b4f8ced1ac221936ec335
 		//Bu options fetch in 2.parametresinde gonderilen data vs icin kullanilan bir objedir
 		const options:optionsType = {
 			method,
 			
 		}
-<<<<<<< HEAD
 		/*Bu body default olarak burda olmamali, cunku, eger direk data cekilecekse o zaman hata alabiliriz,
 		 ve biz body yi optional yapmak istiyoruz ve typescriptin hata vermemsini istiyorsak o zaman da 
 		 optionsType isminde bir type uretirirz ki hata almayalim
@@ -86,18 +71,6 @@ function request(url:string, method="POST", data:any=false):Promise<any>
 			Get fetching is failed! TypeError: Failed to execute 'json' on 'Response': 
 			body stream already read  <script lang="ts">
 			*/
-=======
-		//Bu body default olarak burda olmamali, cunku, eger direk data cekilecekse o zaman hata alabiliriz, ve biz body yi optional yapmak istiyoruz ve typescriptin hata vermemsini istiyorsak o zaman da optionsType isminde bir type uretirirz ki hata almayalim
-		
-		if(data){
-			options.body = JSON.stringify(data);
-		}
-		fetch(url,options)
-		.then(response=>{
-			// console.log("RES-JSON-POST: ", response.json())//Burda console.log ile response.json i yazdiralim deyip sonra da gidip, asagida return edersek o zaman response.json() 2 kez okudgumuz icin , 2.sinde zaten json ile okudun niye bir dha okuyon hatasi verecektir
-			//Get fetching is failed! TypeError: Failed to execute 'json' on 'Response': body stream already read  <script lang="ts">
-
->>>>>>> 8382b31d499b82bef99b4f8ced1ac221936ec335
 			return response.json()
 		})
 		.then(res=>{
@@ -108,7 +81,6 @@ function request(url:string, method="POST", data:any=false):Promise<any>
 	})
 }
 
-<<<<<<< HEAD
 /*
 Requestimizi neden tek yerdden yonetiyoruz cunku yarin oburgun hata durumunu
  yonetirken ornegn Toaster library i kullanmak istedik, ve o zaman ornegin 
@@ -125,9 +97,6 @@ Hem yukarda hem de burda fonksiyonlari Promise<any> type olarak dondurmemiz cooo
    olarak algilayip bu seferde diyorki sen unknown bir type da datayi listeleyemezzsin 
 	hatasi veriyor,bu nu anlamak cook onemlidir
 */
-=======
-//Hem yukarda hem de burda fonksiyonlari Promise<any> type olarak dondurmemiz coook onemli yoksa, biz bu fonksionlari kullanip bu fonksiyonlardan gelecek data yi each ile yazidrmaya calisitimgzdan dolayi , ve bu fonksiyonlara return type vermeyince onu typescript unknown olarak algilayip bu seferde diyorki sen unknown bir type da datayi listeleyemezzsin hatasi veriyor,bu nu anlamak cook onemlidir
->>>>>>> 8382b31d499b82bef99b4f8ced1ac221936ec335
 export const post = (url:string,data:any=false):any=>request(url,"POST",data);
 export const get = (url:string,data:any=false):Promise<any>=>{
 	return request(url,"GET",data);
